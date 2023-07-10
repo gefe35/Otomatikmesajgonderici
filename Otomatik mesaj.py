ff85763ad61d2e8f2ab92c5cbb249f16944180dd
Goktug Efe <95084375+gefe35@users.noreply.github.com>
2023-07-10 23:14:48 +0300

    Update Otomatik mesaj.py

--- Otomatik mesaj.py
+++ Otomatik mesaj.py
@@ -1,43 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+import pywhatkit
+import datetime
 
-# WhatsApp Web sayfasını aç
-from selenium import webdriver
+def send_whatsapp_message(number, message, hour, minute):
+    now = datetime.datetime.now()
+    current_hour = now.hour
+    current_minute = now.minute
 
-# WebDriver dosyasının konumunu belirtin
-driver_path = "path/to/chromedriver.exe"
+    pywhatkit.sendwhatmsg(number, message, current_hour, current_minute + 1)
 
-# WebDriver'ı başlatın
-driver = webdriver.Chrome(executable_path=driver_path)
+# Kullanımı:
+send_whatsapp_message("+9000000000", "Merhaba, bu bir otomatik mesajdır.", 9, 30)
+import pywhatkit
 
-# Kodunuzun geri kalanını buraya ekleyin
-# ...
+def send_whatsapp_message(number, message, hour, minute):
+    pywhatkit.sendwhatmsg(number, message, hour, minute)
 
-# WebDriver'ı kapatın
-driver.quit()
-
-driver.get('https://web.whatsapp.com')
-
-# Kullanıcıya WhatsApp QR kodunu taraması için zaman ver
-time.sleep(15)
-
-# Mesaj göndermek istediğiniz kişinin adını veya grubun adını burada belirtin
-receiver_name = "Alıcı İsmi"
-
-# Mesaj içeriğini burada belirtin
-message_content = "Merhaba! Bu bir otomatik mesajdır."
-
-# Alıcının veya grubun bulunduğu elementi bulmak için XPATH kullanarak arama yapın
-search_box = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="3"]')
-search_box.send_keys(receiver_name)
-search_box.send_keys(Keys.ENTER)
-
-# Mesaj kutusunu bul ve mesajı gönder
-message_box = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="1"]')
-message_box.send_keys(message_content)
-message_box.send_keys(Keys.ENTER)
-
-# İşlem tamamlandıktan sonra tarayıcıyı kapat
-time.sleep(5)
-driver.quit()
+# Kullanımı:
+send_whatsapp_message("+900000000", "Merhaba, bu bir otomatik mesajdır.", 9, 30)
